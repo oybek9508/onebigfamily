ec8a38c5349fa1ac616d84d0c2561130604b801c
Oybek <oybek@marvrus.com>
2023-07-06 03:25:36 +0900

    fix: fix header

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import Sticky from "../common/Sticky";
 import { Grid } from "@mui/material";
 import MobileBottomNavigation from "../mobile-navigation/MobileBottomNavigation";
-import Header from "../Header";
-import Footer from "../Footer";
+import Header from "../header";
+import Footer from "../footer";
 
 const Layout = ({ children, isFixed }) => {
 	return (
