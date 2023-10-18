dc8bd8058425d0577fdd1308b9792d3954d564ce
Oybek <oybek@marvrus.com>
2023-10-18 21:51:22 +0900

    feat: change the mete property title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,10 +14,10 @@ export default class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 					/>
 					<meta name="naver-site-verification" content="e8afd070701fb8fd8bc12375e6db6498d4f7ce14" />
-					<meta name="description" content="메타버스와 함께하는 모빌리티(Mobility)을 만나보세요." />
+					<meta name="description" content="Big Way Trading" />
 					<meta property="og:type" content="website" />
-					<meta property="og:title" content="모빌리티 랜드" />
-					<meta property="og:description" content="메타버스와 함께하는 모빌리티(Mobility)을 만나보세요." />
+					<meta property="og:title" content="Big Way Trading" />
+					<meta property="og:description" content="Big Way Trading" />
 				</Head>
 				<body>
 					<Main />
