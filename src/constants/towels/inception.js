44299e0e7aeb5d011f69037b12b3aafb9a651968
Oybek <oybek@marvrus.com>
2023-11-30 22:37:43 +0900

    feat: add inception 4,5,6 towel sets

--- src/constants/towels/inception.js
+++ src/constants/towels/inception.js
@@ -8,9 +8,15 @@ const { arr6 } = imgArrs;
 const images1 = imageArrays(arr6, 1, filePath, fileType);
 const images2 = imageArrays(arr6, 2, filePath, fileType);
 const images3 = imageArrays(arr6, 3, filePath, fileType);
+const images4 = imageArrays(arr6, 4, filePath, fileType);
+const images5 = imageArrays(arr6, 5, filePath, fileType);
+const images6 = imageArrays(arr6, 6, filePath, fileType);
 
 export const inceptionImages = {
 	images1,
 	images2,
 	images3,
+	images4,
+	images5,
+	images6,
 };
--- src/constants/towels/subtitle/inception.js
+++ src/constants/towels/subtitle/inception.js
@@ -2,4 +2,7 @@ export const inceptionSubtitle = {
 	inception1: "Terry towel design - 100% cotton towels with elegant three-line border, size: 50x90 cm, 450 GSM",
 	inception2: "Terry towel design - 100% cotton towels with elegant three-line border, size: 70x140 cm, 450 GSM",
 	inception3: "Terry towel design - 100% cotton towels with elegant three-line border, size: 100x180 cm, 450 GSM",
+	inception4: "Terry towel design - 100% cotton towels with elegant three-line border, size: 100x180 cm, 500 GSM",
+	inception5: "Terry towel design - 100% cotton towels with elegant three-line border, size: 100x180 cm, 500 GSM",
+	inception6: "Terry towel design - 100% cotton towels with elegant three-line border, size: 100x180 cm, 500 GSM",
 };
